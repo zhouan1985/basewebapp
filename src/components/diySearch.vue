151af3596d15f42b60c060dc7616694da7b12b2e
zhoua <263534528@qq.com>
2020-04-02 11:04:15 +0800

    搜索组件

---
+++ src/components/diySearch.vue
@@ -0,0 +1,34 @@
+<template>
+    <van-search
+         ref="vansearch"
+        :background="diyStyle.warpBg"
+        :input-align="diyStyle.textStyle.textAlign"
+        :placeholder="diyStyle.searchText"
+        :shape="diyStyle.borderRadius == '0' ? 'square' : 'round'"
+        v-model="value"
+    />
+</template>
+
+<script>
+    export default {
+        name: "diySearch",
+        props:{
+            diyStyle:{
+                type: Object,
+                default: null
+            }
+        },
+        data() {
+            return {
+                value: ''
+            }
+        },
+        mounted(){
+           console.log(this.$refs.vansearch)
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,19 +1,39 @@
 <template>
     <div class="home">
-        <van-row>
-            <van-col span="8">span: 8</van-col>
-            <van-col span="8">span: 8</van-col>
-            <van-col span="8">span: 8</van-col>
-        </van-row>
+       <div v-for="(item,index) in diyComponents" :key="index">
+           <diy-search :diyStyle="item" v-if="item.type=='search'"/>
+       </div>
     </div>
 </template>
 
 <script>
+import diySearch from '@/components/diySearch'
 export default {
     data(){
         return {
-
+            // pageType:1:系统页面-首页，2：系统页面-会员中心，3系统页面-商品详情页，应用：4分销中心，5积分商城，6整点秒杀，7兑换中心
+            pageType: '',
+            diyComponents: [
+                {
+                    type: 'search',
+                    searchType: '1', // 搜索类型
+                    warpBg: '#fff', // 搜素框外围背景
+                    inputBg: '#999999', // 输入框背景
+                    textStyle: { // 文字样式
+                        color: '#fff',
+                        textAlign: 'left'
+                    },
+                    iconColor: '#b4b4b4', // 图标颜色
+                    searchText: '请输入关键字进行搜素', // 搜素的文字
+                    paddingUd: 10, // 上下边距
+                    paddingLr: 10, // 左右边距
+                    borderRadius: '10' // 搜索框圆角
+                },
+            ]
         }
+    },
+    components: {
+        diySearch
     },
     methods:{
 
@@ -24,6 +44,6 @@ export default {
   .home{
       width: 100%;
       height: 100%;
-      padding: 0 10px;
+      /*padding: 0 10px;*/
   }
 </style>
